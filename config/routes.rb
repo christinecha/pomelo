6b66d8eb249c2c55a4887fb79119154cde2512cd
James O'Dwyer <j.odwyer320@gmail.com>
2015-08-23 01:49:58 -0400

    routed pages. navbar clean up

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resources :quotes
   root 'static#home'
-
+  get 'team', to: 'static#team'
+  get 'services', to: 'static#services'
+  get 'contact', to: 'static#contact'
 end
