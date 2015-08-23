fcc1fe460a6687501e428e381418f989ea0c7f2d
CHRISTINE CHA <HELLO@CHRISTINECHA.COM>
2015-08-23 00:40:45 -0400

    Initial commit.

---
+++ app/assets/javascripts/type-animation.js
@@ -0,0 +1,48 @@
+$(document).ready(function(){
+  var words = ["impress", "sell", "excite", "wow", "last"];
+  var index = 0;
+  var word = words[index];
+  var progress = 0;
+
+  // var changeWord = function(){
+  //   if (index < words.length){
+  //     index += 1;
+  //   } else {
+  //     index = 0;
+  //   };
+  //   word = words[index];
+  //   console.log("now the word is" + word);
+  // };
+
+  var showWord = function(){
+    word = words[index].slice(0, progress);
+    $('#type-animation').html(word);
+    typing = true;
+    progress+= 1;
+    console.log(word);
+    if (progress === words[index].length + 1){
+      clearInterval(handleShow);
+    }
+  };
+
+  var handleShow = setInterval(showWord, 200);
+
+  // var deleteWord = function(){
+  //   word = words[index].slice(0, progress);
+  //   $('#type-animation').html(word);
+  //   typing = true;
+  //   progress-= 1;
+  //   console.log(word);
+  //   if (progress === 0){
+  //     clearInterval(handleShow);
+  //   }
+  // };
+  //
+  // var handleDelete = setInterval(deleteWord, 200);
+
+  if (progress < words[index].length + 1){
+    handleShow;
+  };
+
+
+});
---
+++ app/controllers/static_controller.rb
@@ -0,0 +1,16 @@
+class StaticController < ApplicationController
+  protect_from_forgery with: :exception
+
+  def home
+  end
+
+  def team
+  end
+
+  def services
+  end
+
+  def contact
+  end
+
+end
